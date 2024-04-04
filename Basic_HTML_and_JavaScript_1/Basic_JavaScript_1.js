a8a6ce1c98cee7025d8d486581bbe25d7fc62244
ReevesMyra <154421142+ReevesMyra@users.noreply.github.com>
2024-04-04 00:25:35 -0700

    Second half of Step 120

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,1 +1,9 @@
+// Step 108
 alert("Hello! I am an alert popup box!");
+
+// Step 120
+function Saint_Patty_Day() {
+    var String = "🍀 Happy Saint Patrick's Day! 🍀";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML =  result;
+}
